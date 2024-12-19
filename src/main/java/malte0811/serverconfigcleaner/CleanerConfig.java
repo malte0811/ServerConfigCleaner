959d406890818cf707076cdb588d2984a155e027
malte0811 <malte0811@web.de>
2024-12-19 12:20:12 +0100

    Add some known false positives

--- src/main/java/malte0811/serverconfigcleaner/CleanerConfig.java
+++ src/main/java/malte0811/serverconfigcleaner/CleanerConfig.java
@@ -8,6 +8,19 @@ import java.util.List;
 public class CleanerConfig {
     public static final ModConfigSpec CONFIG_SPEC;
 
+    private static final List<String> DEFAULT_FALSE_POSITIVES = ImmutableList.of(
+            "computercraft:http.proxy.host",
+            "rftoolsbuilder:scanner.locatorUsePerTickHostile",
+            "tombstone:player_death.ghostly_shape_duration",
+            // mahoutsukai for some reason puts essentially all config values in "secret", even though none of them are
+            // secret in any sense
+            "mahoutsukai:secret.*"
+    );
+    private static final List<Integer> KNOWN_PROBLEMATIC_HASHES = ImmutableList.of(358182576);
+    private static final List<String> DEFAULT_SUSPICIOUS_PATTERNS = ImmutableList.of(
+        "host", "username", "password", "secret", "token", "apikey", "webhook", "jdbc", "sql", "redis", "mongodb"
+    );
+
     public static final ModConfigSpec.ConfigValue<List<? extends String>> BAD_CONFIG_PATTERNS;
     public static final ModConfigSpec.ConfigValue<List<? extends String>> TRUE_PROBLEMATIC_CONFIGS;
     public static final ModConfigSpec.ConfigValue<List<? extends String>> FALSE_POSITIVES;
@@ -18,9 +31,7 @@ public class CleanerConfig {
         ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
         BAD_CONFIG_PATTERNS = builder
                 .comment("Patterns used to determine \"suspicious\" config keys. Capitalization is ignored.")
-                .defineList("badConfigPatterns", ImmutableList.of(
-                        "host", "username", "password", "secret", "token", "apikey", "webhook", "jdbc", "sql", "redis", "mongodb"
-                ), obj -> true);
+                .defineList("badConfigPatterns", DEFAULT_SUSPICIOUS_PATTERNS, obj -> true);
         TRUE_PROBLEMATIC_CONFIGS = builder
                 .comment("Options to exclude from config syncing", "Format: modid:category.configKey")
                 .defineListAllowEmpty("doNotSync", ImmutableList.of(), obj -> true);
@@ -29,13 +40,13 @@ public class CleanerConfig {
                         "Options that are detected by the badConfigPatterns, but should still be synced",
                         "Format: modid:category.configKey; * allowed at the end of an entry"
                 )
-                .defineListAllowEmpty("falsePositives", ImmutableList.of(), obj -> true);
+                .defineListAllowEmpty("falsePositives", DEFAULT_FALSE_POSITIVES, obj -> true);
         PROBLEMATIC_HASHES = builder
                 .comment(
                         "An \"obfuscated\" list of known options containing secrets.",
                         "Ignore unless you know what you are doing."
                 )
-                .defineListAllowEmpty("doNotSyncHashes", ImmutableList.of(358182576), obj -> true);
+                .defineListAllowEmpty("doNotSyncHashes", KNOWN_PROBLEMATIC_HASHES, obj -> true);
 
         CONFIG_SPEC = builder.build();
     }
